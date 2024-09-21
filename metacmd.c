4834e2f539984c152ca5a9f5172e0c403b74f289
ppaprikaa <sanzhar.maratov@proton.me>
2024-09-22 03:36:37 +0500

    rename function to fix build

--- metacmd.c
+++ metacmd.c
@@ -1,7 +1,7 @@
 #include "metacmd.h"
 #include "string.h"
 
-meta_cmd_result do_meta_command(char *meta_command) {
+meta_cmd_result do_meta_cmd(char *meta_command) {
 	if (!strcmp(meta_command, ".exit")) {
 		return META_CMD_EXIT;
 	} 
