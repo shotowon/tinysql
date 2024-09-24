8f8eec97d63db06dd53cc889072de666692f7d11
ppaprikaa <sanzhar.maratov@proton.me>
2024-09-24 22:30:54 +0500

    table definitions

---
+++ table.h
@@ -0,0 +1,13 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+// for now table will be hardcoded
+// columns:
+// id: integer
+// username: varchar(32)
+// email: varchar(255)
+
+#define COL_USERNAME_LENGTH 32
+#define COL_EMAIL_LENGTH 255
+
+#endif
