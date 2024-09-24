83a027468f8be297878af751768035322a691bb3
ppaprikaa <sanzhar.maratov@proton.me>
2024-09-24 22:32:34 +0500

    add row into stmt for insert stmt support

--- stmt.h
+++ stmt.h
@@ -1,6 +1,8 @@
 #ifndef STMT_H
 #define STMT_H
 
+#include "table.h"
+
 typedef enum {
 	PREPARE_SUCCESS, 
 	PREPARE_UNRECOGNIZED_STMT
@@ -13,6 +15,7 @@ typedef enum {
 
 typedef struct {
 	stmt_type type;
+	row row_to_insert; // only used by insert stmt
 } stmt;
 
 prepare_stmt_result prepare_stmt(char *stmt_str, stmt *s);
